d7bff05682a52e9c7903fe1a4e4e308757ce4006
Rolando <rolandorop04@gmail.com>
2024-11-21 19:51:12 +0100

    "Añadido  mensaje de bienvenida"

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo desde IntelliJ IDEA!");;
+        System.out.println("¡Hola, mundo desde IntelliJ IDEA!");
         System.out.println("Bienvenidos al curso de DAM");
     }
 }
